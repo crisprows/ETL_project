d1771b52deefdde63339cce86e1dd3a959fd83e5
BillRigg <riggiobill@gmail.com>
2020-10-20 23:36:07 -0400

    CSV and Resources Folder
    CSV of word list information and resources folder to contain it.

--- Bill - SEO List/filter.py
+++ Bill - SEO List/filter.py
@@ -2,3 +2,32 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 from splinter import Browser
+
+def init_browser():
+    executable_path = {"executable_path": "chromedriver"}
+    return Browser("chrome",**executable_path, headless=False)
+
+def scrape():
+    browser = init_browser()
+
+    SEO_list = []
+
+
+    url = "https://app.wordstream.com/fkt/app?cid=Web_Any_FreeTool_PopKW_Banner_KeywordTool&ref=undefined"
+    browser.visit(url)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    #news = soup.find("div", class_="list_text")
+    #news_paragraph = news.find("div", class_="article_teaser_body").text
+    #news_title = news.find("div",class_="content_title").text
+
+    test = soup.find("div", class_="col-xs-2 cell-border-left")
+    test1 = test.find("span", class_="gridKeywordClass").text
+
+    print(test1)
+
+
+scrape()
+
