c3fa640296e78f156f70bcbe34f4ea55373daa19
BillRigg <riggiobill@gmail.com>
2020-10-22 19:28:54 -0400

    def Keyword_List
    First function done, second function started.

--- Bill - SEO List/filter.py
+++ Bill - SEO List/filter.py
@@ -3,31 +3,40 @@ import requests
 import pandas as pd
 from splinter import Browser
 
-def init_browser():
-    executable_path = {"executable_path": "chromedriver"}
-    return Browser("chrome",**executable_path, headless=False)
 
-def scrape():
-    browser = init_browser()
+# Created a function to generate a keyword list of custom size
+def keyword_list(amount):
+    keywords_csv = "Resources/Wordstream_pet_NJ.csv"
+    keywords_df = pd.read_csv(r'Resources/Wordstream_pet_NJ.csv')
+    print("csv has been read.")
 
-    SEO_list = []
-
-
-    url = "https://app.wordstream.com/fkt/app?cid=Web_Any_FreeTool_PopKW_Banner_KeywordTool&ref=undefined"
-    browser.visit(url)
-
-    html = browser.html
-    soup = bs(html, "html.parser")
-
-    #news = soup.find("div", class_="list_text")
-    #news_paragraph = news.find("div", class_="article_teaser_body").text
-    #news_title = news.find("div",class_="content_title").text
-
-    test = soup.find("div", class_="col-xs-2 cell-border-left")
-    test1 = test.find("span", class_="gridKeywordClass").text
-
-    print(test1)
+    keyword_list = []
+    
 
 
-scrape()
+    for x in range(amount):
+        print("Appending keyword to list...")
+        keyword_list.append(keywords_df['Keyword'][x])
 
+    return keyword_list
+
+
+
+def keyword_scan(dataset, keyword_list):
+    #for every cell(word) in dataset
+    ratingcount = 0
+
+    for word in dataset
+    #see if word is in keyword list
+    if keyword_list.contains(word):
+        ratingcount = ratingcount + 1
+        #if word is in keyword list:
+        # add to ratings count for dataset and then break/pass
+    #   #end if
+    # 
+    #after for loop, return keyword rating - 
+    #which is, amount of times the word was in the keyword_list
+
+
+# A test call to the keyword function.
+print(keyword_list(10))
